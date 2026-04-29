168fadc3af511db7602fa8ab59e5087c444f444e
Enzo Di Maiolo <edi-maio@3F1.42angouleme.fr>
2026-04-29 15:47:40 +0200

    commit message

--- ex01/srcs/main.cpp
+++ ex01/srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: edi-maio <edi-maio@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/01 21:06:24 by edi-maio          #+#    #+#             */
-/*   Updated: 2026/04/24 17:54:49 by edi-maio         ###   ########.fr       */
+/*   Updated: 2026/04/29 15:46:25 by edi-maio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,11 @@ int	main(void)
 {
 	Zombie	*horde;
 
-	horde = zombieHorde(10, "Zomboss");
+	horde = zombieHorde(200, "Zomboss");
+	if (!horde)
+		return (1);
 	for (int i = 0; i < 10; i++)
 		horde[i].announce();
 	delete[] horde;
+	return (0);
 }
--- ex01/srcs/zombieHorde.cpp
+++ ex01/srcs/zombieHorde.cpp
@@ -6,7 +6,7 @@
 /*   By: edi-maio <edi-maio@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/01 21:46:24 by edi-maio          #+#    #+#             */
-/*   Updated: 2026/04/22 19:11:00 by edi-maio         ###   ########.fr       */
+/*   Updated: 2026/04/29 15:45:55 by edi-maio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,14 @@
 
 Zombie*	zombieHorde(int N, std::string name)
 {
-	Zombie	*horde = new Zombie[N];
+	Zombie	*horde;
 
+	if (N < 1 || N > 200)
+	{
+		std::cout << "can't create " << N << " zombies" << std::endl;
+		return (NULL);
+	}
+	horde = new Zombie[N];
 	std::cout << "creation of " << N << " zombies named " << name << std::endl;
 	for (int i = 0; i < N; i++)
 		horde[i].setName(name);
